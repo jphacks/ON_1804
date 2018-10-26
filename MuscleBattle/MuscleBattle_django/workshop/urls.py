1ffd90ee53b64f02eb6047e585f88fece6d3db41
shigenius <k178578@ie.u-ryukyu.ac.jp>
2018-10-26 19:03:44 +0900

    add my intro-page

---
+++ MuscleBattle/MuscleBattle_django/workshop/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('intro/', views.intro, name='intro'),
+    path('shumi/', views.shumi, name='shumi')
+]
---
+++ MuscleBattle/MuscleBattle_django/workshop/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def intro(request):
+    return render(request, 'intro/index.html')
+
+def shumi(request):
+    return render(request, 'shumi/index.html')

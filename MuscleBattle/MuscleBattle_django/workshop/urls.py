5f8c1a9f87b0af60280358ab9959cb789ff18677
KentaroMaeshiro <maeken2010@gmail.com>
2018-10-26 19:03:34 +0900

    自分の自己紹介ページを追加

---
+++ MuscleBattle/MuscleBattle_django/workshop/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+app_name = 'workshop'
+urlpatterns = [
+    path('intro/', views.intro, name='intro'),
+    path('syumi/', views.syumi, name='syumi')
+]
---
+++ MuscleBattle/MuscleBattle_django/workshop/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def intro(request):
+    return render(request, 'intro/index.html')
+
+def syumi(request):
+    return render(request, 'intro/syumi.html')

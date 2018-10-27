0644a66015995bbcbfce1b3836ab0ee32b3217b1
Reiya Dakujaku <e185709@ie.u-ryukyu.ac.jp>
2018-10-27 15:14:54 +0900

    ログイン画面の追加

--- MuscleBattle/MuscleBattle_django/app/views.py
+++ MuscleBattle/MuscleBattle_django/app/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib.auth.views import (
+    LoginView, LogoutView
+)
+from .forms import LoginForm
 
-# Create your views here.
+
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'regstration/signup.html'
+
+class Login(LoginView):
+    """ログインページ"""
+    form_class = LoginForm
+    template_name = 'regstration/login.html'

1f81eb06f616bc5cce1df3d24914705c432a3fc4
Keitaro <e185748@ie.u-ryukyu.ac.jp>
2018-10-27 15:42:28 +0900

    rankingを追加

--- MuscleBattle/MuscleBattle_django/MuscleBattle_django/settings.py
+++ MuscleBattle/MuscleBattle_django/MuscleBattle_django/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'home.apps.HomeConfig',
+    'ranking.apps.RankingConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- MuscleBattle/MuscleBattle_django/MuscleBattle_django/urls.py
+++ MuscleBattle/MuscleBattle_django/MuscleBattle_django/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', include('home.urls')),
+    path('ranking/', include('ranking.urls')),
 ]

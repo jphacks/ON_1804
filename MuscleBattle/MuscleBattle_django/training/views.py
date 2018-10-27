d249b5d28f23da38b8a5043bc474a2cde49bbf38
KentaroMaeshiro <maeken2010@gmail.com>
2018-10-28 08:44:27 +0900

    modelとtemplates更新

--- MuscleBattle/MuscleBattle_django/training/views.py
+++ MuscleBattle/MuscleBattle_django/training/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, get_list_or_404
 from .models import TrainingMenu
+from django.db.models import Q
 
 def menu(request, week_id):
-    training_menu = get_list_or_404(TrainingMenu, week=week_id)
+    try:
+        training_menu = TrainingMenu.objects.filter(Q(week1=week_id) | Q(week2=week_id))
+    except TrainingMenu.DoesNotExist:
+        raise Http404("Question does not exist")
     context = {'training_menu': training_menu}
     return render(request, 'training/menu.html', context)
